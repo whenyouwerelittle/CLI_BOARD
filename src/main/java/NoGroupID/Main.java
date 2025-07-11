2b73eaada2e91cad764a2d5f5486846dd678542c
whenyouwerelittle <whenyouwerelittle@gmail.com>
2025-07-11 17:29:48 +0900

    stage 1

--- src/main/java/NoGroupID/Main.java
+++ src/main/java/NoGroupID/Main.java
@@ -1,17 +1,43 @@
-package NoGroupID;
-
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+// package NoGroupID;
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        /*
+            종료
+            ==게시판앱
+            명령 종료
+        */
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("---게시글---");
+        while(true){
+            System.out.print("입력: ");
+            String input = scanner.nextLine();
+            if(input.equals("종료")){
+                break;
+            }
         }
+        /*
+        int sum = 0;
+        String input;
+
+        System.out.println("글을 입력하세요 (종료하려면 0 입력):");
+
+        while (true) {
+            System.out.print("입력: ");
+            input = scanner.nextLine();
+            if (input.equalsIgnoreCase(0)) {
+                break; // "exit" 입력 시 반복 종료
+            }
+
+            try {
+                String message = Integer.parseInt(input);
+                // sum += number;
+            } catch (NumberFormatException e) {
+                System.out.println("유효하지 않은 숫자입니다. 다시 입력해주세요.");
+            }
+        }
+
+        System.out.println("총 합계: " + sum);*/
+        scanner.close();
     }
 }
